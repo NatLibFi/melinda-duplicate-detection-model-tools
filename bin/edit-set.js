13ff0e1f695b8cad0f8182167ea16bcaab42de52
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-21 08:32:22 +0300

    Adds title, partition and halve commands

--- bin/edit-set.js
+++ bin/edit-set.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
-
+const fs = require('fs');
 const _ = require('lodash');
 const MarcRecord = require('marc-record-js');
 
-const AVAILABLE_COMMANDS = ['remove', 'count', 'tostring', 'fromstring', 'labels'];
+const AVAILABLE_COMMANDS = ['remove', 'count', 'tostring', 'fromstring', 'labels', 'titles', 'partition', 'halve'];
 
 try {
   const { command, pairs } = readArguments(process.argv);
@@ -20,7 +20,6 @@ try {
 async function run(command, pairs) {
 
   const recordSetData = await readStdin();
-
 
   let recordSet;
   try {
@@ -67,15 +66,20 @@ function transformSet(recordSet, command, pairs) {
     }).join('\n\n\n');
   }
   if (command === 'fromstring') {
-    return recordSet.split('\n\n\n').map(itemStr => {
-      const [labelLine, record1Str, record2Str] = itemStr.split('\n\n');
-      const label = labelLine.substr(7);
-      const record1 = MarcRecord.fromString(record1Str.trim());
-      const record2 = MarcRecord.fromString(record2Str.trim());
-      return {
-        label,
-        pair: { record1, record2 }
-      };
+    return recordSet.split('\n\n\n').filter(str => str.length > 10).map(itemStr => {
+      try {
+        const [labelLine, record1Str, record2Str] = itemStr.split('\n\n');
+        const label = labelLine.substr(7);
+        const record1 = MarcRecord.fromString(record1Str.trim());
+        const record2 = MarcRecord.fromString(record2Str.trim());
+        return {
+          label,
+          pair: { record1, record2 }
+        };
+      } catch(error) {
+        console.log(itemStr);
+        throw error;
+      }
     });
   }
 
@@ -89,6 +93,43 @@ function transformSet(recordSet, command, pairs) {
       return `${item.label}\t${id1}\t${id2}`;
     }).join('\n');
   }
+
+  if (command === 'titles') {
+    const getTitle = getField('245');
+    return recordSet.map(item => {
+      
+      const title1 = getTitle(item.pair.record1);
+      const title2 = getTitle(item.pair.record2);
+
+      return `${item.label}\t${fieldToString(title1)}\t${fieldToString(title2)}`;
+    }).join('\n');
+  }
+
+  if (command === 'partition') {
+
+    shuffle(recordSet);
+
+    const setSize = Math.floor(recordSet.length * 0.15);
+
+    const crossValidationSet = recordSet.splice(0,setSize);
+    const testSet = recordSet.splice(0,setSize);
+    const trainingSet = recordSet;
+
+    fs.writeFileSync('crossValidationSet.json', JSON.stringify(crossValidationSet));
+    fs.writeFileSync('testSet.json', JSON.stringify(testSet));
+    fs.writeFileSync('trainingSet.json', JSON.stringify(trainingSet));
+    
+    process.exit(0);
+  }
+
+  if (command === 'halve') {
+
+    const half = Math.floor(recordSet.length * 0.5);
+    return recordSet.splice(0, half);
+  
+  }
+
+  
 
   throw new Error(`Command ${command} not implemented.`);
 }
@@ -114,6 +155,7 @@ function getRecordId(record) {
   const f001 = _.head(record.fields.filter(field => field.tag === '001'));
   return _.get(f001, 'value');
 }
+const getField = tag => record => _.head(record.fields.filter(field => field.tag === tag));
 
 function readArguments(argv) {
 
@@ -159,4 +201,39 @@ async function readStdin() {
       resolve(data);
     });
   });
+}
+
+
+
+function fieldToString(field) {
+  if (field && field.subfields) {
+    return dataFieldToString(field);
+  } else {
+    return controlfieldToString(field);
+  }
+
+  function dataFieldToString(field) {
+    const ind1 = field.ind1 || ' ';
+    const ind2 = field.ind2 || ' ';
+    const subfields = field.subfields.map(subfield => `‡${subfield.code}${subfield.value}`).join('');
+    return `${field.tag} ${ind1}${ind2} ${subfields}`;
+  }
+
+  function controlfieldToString(field) {
+    return `${field.tag}    ${field.value}`;
+  }
+}
+
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
