93a60bdeb1c11900a173cd14ee22e7c7746785da
Pasi Tuominen <pasi.tuominen@gmail.com>
2017-09-23 13:53:43 +0300

    Read files from tmp

--- bin/extract-false-positives.js
+++ bin/extract-false-positives.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const Utils = require('melinda-deduplication-common/similarity/utils');
 const items = JSON.parse(fs.readFileSync('/tmp/guesses.json'));
 
-const filename = 'falsePositives.json';
+const filename = 'tmp/falsePositives.json';
 
 const falsePositives = items.filter(item => item.type === Utils.Types.FALSE_POSITIVE);
 console.log(falsePositives.length);
